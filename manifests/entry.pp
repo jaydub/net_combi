4c01f99cc11caf3d26d12242d3344ddb5fb00339
John Morton <jwm@angrymonkey.net.nz>
2016-01-21 02:28:39 +1300

    Make MAC undef by default, and arp/dhcp contingent on it having a value. Improve comments

--- manifests/entry.pp
+++ manifests/entry.pp
@@ -4,22 +4,31 @@
 # dns entries.
 
 define net_combi::entry (
-  $mac='',
-  $ip='',
+  $ip,
+  $mac=undef,
   $fqdn=$title,
   $aliases=[],
   $domain='',
   $resource_creation = 'export'
 )
 {
-  $norm_domain = regsubst($domain, '^[^\.].+', ".\0")
+  # Must have an IP. Mac is optional; without it, arp and dhcp entries
+  # aren't created, but DNS and hosts entries are, which is useful for entries
+  # outside of the subnet. 
+
+  # Put a dot in front of the domain, if it doesn't have one  
+  $norm_domain = regsubst($domain, '^[^\.].+', '.\0')
+  # The hostname aliases, with the domain appended
   $fqdn_aliases = suffix($aliases, $norm_domain)
+  # Make sure the FQDN is not duplicated in the DNS list.
   $tinydns_aliases = delete($fqdn_aliases, $fqdn)
 
-  # BUG: Work around pass by reference brain damage in the paser function
+  # BUG: Work around pass by reference brain damage in the parser function
   # interface that results in eg concat modifying it's input as a
-  # side effect. 
+  # side effect. Probably fixed by puppet 4.0 
   $ablative_tinydns_aliases =  suffix($tinydns_aliases, '')
+  
+  # Hosts have both short and long names
   $host_aliases = concat($ablative_tinydns_aliases, $aliases)
 
   $static_arp_data = {
@@ -47,9 +56,12 @@ define net_combi::entry (
     default:   {$prefix = ''}
   }
   validate_array($tinydns_aliases)
-  create_resources("${prefix}static_arp::entry", $static_arp_data)
+
+  if $mac != undef {
+    create_resources("${prefix}static_arp::entry", $static_arp_data)
+    # host reservations use the same data as static arp entries.
+    create_resources("${prefix}dhcp::host", $static_arp_data)
+  }
   create_resources("${prefix}host", $host_data)
-  # host reservations use the same data as static arp entries.
-  create_resources("${prefix}dhcp::host", $static_arp_data)
   create_resources("${prefix}dbndns::tinydns_host", $tinydns_data)
 }
